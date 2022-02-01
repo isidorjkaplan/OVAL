a6c974b6f175876e11bac03093ad6dd8d8ddfebb
Ryan Ghosh <ryan.ghosh21@gmail.com>
2022-02-01 14:51:06 -0500

    initial model architecture, very rough, no testing done, just checkpointing

--- offline.py
+++ offline.py
@@ -4,7 +4,7 @@
 # This function will train on large batches of data consisting of many videos
 # The goal of offline training is to produce the most general possible model to start with
 # Then each time a video call happens they retrain the offline model to overfit to their own scenario
-# 
+#
 # INPUT: An autoencoder=(encoder/decoder), dataset=(train/validation) sets, and hyperparameters
 #        Note for this training we train on a large volume of complete videos, this is the pre-training
 # OUTPUT: Trained autoencoder, graph / history of training
