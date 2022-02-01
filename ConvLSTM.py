6ac9a44a29230abac000e0c624291a12bc77df81
Ryan Ghosh <ryan.ghosh21@gmail.com>
2022-02-01 16:15:38 -0500

    Started implementation of ConvLSTM Autoencoder model. Creating checkpoint

---
+++ ConvLSTM.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+import torch
+
+
+class ConvLSTMCell(nn.Module):
+    ## inspired by https://proceedings.neurips.cc/paper/2015/file/07563a3fe3bbe7e3ba84431ad9d055af-Paper.pdf
+    ## and Andreas Holm Nielsen for ConvLSTM Cell code. 
+    def __init__(self, input_dim:int, hidden_dim:int, kernel_size:int, bias:bool):
+        """
+        input_dim: Number of input channels.
+        hidden_dim: Number of hidden channels.
+        kernel_size: Size of the convolutional kernel.
+        bias: Choose to add bias.
+        """
+
+        super(ConvLSTMCell, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
+from ConvLSTM import ConvLSTMCell
 
 #Autoencoder = namedtuple("Autoencoder", "encoder decoder")
 
@@ -12,46 +13,23 @@ import torch.optim as optim
 # Enc_Sizes = Array of possible encoding size choices
 class Autoencoder():
     def __init__(self, num_enc_layers):
-        self.encoder = Encoder(mode, num_enc_layers)
-        self.decoder = Decoder(mode, num_enc_layers)
+        self.encoder = Encoder("default", num_enc_layers)
+        self.decoder = Decoder("default", num_enc_layers)
 
 
 class Encoder(nn.Module):
-    def __init__(self, mode:str, num_enc_layers:int, initial_dim):
+    def __init__(self, mode:str, num_enc_layers:int, num_frames:int, input_dim):
         super().__init__()
-        #Define convolutional layers for preprocessing
-        self.preproc_conv_net = []  ## TODO
-        ## TODO look into convutional LSTM, variable encoding
-        #Define an RNN to hold the memory state, this is based on the convolutional features
-        self.rnn = nn.Sequential(
-            nn.LSTM(750, 1000, num_layers=3),
-            nn.Linear(1000, 256),
-            nn.Linear(256,128)
-        ) # TODO
+        self.encoder_1_convlstm = ConvLSTMCell(input_dim=input_dim,
+                                               hidden_dim=num_frames,
+                                               kernel_size=(3, 3),
+                                               bias=True)
 
-        #Define the autoencoder layers that reduce size down to enc_sizes[0]
-        # This is some more potentially convolutional, not sure, preprocessing
-        self.num_enc_layers = num_enc_layers #Save for use in forward
-        #TODO Make sure to use CONVOLUTIONAL encoding layers, this will require some clever work
-
-        encoded_space_dim = 5   ## change to actual dimension
-
-        self.layers = nn.Sequential(
-            nn.Conv2d(initial_dim[0],initial_dim[1],initial_dim[2],stride=2,padding=1),     ## dummy dimensions, fill in later
-            nn.ReLU(True),
-            nn.Conv2d(initial_dim[1],16,3, stride=2,padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            nn.Conv2d(16, 32, 3, stride=2, padding=0),
-            nn.ReLU(True),
-            nn.Flatten(start_dim=1),
-            nn.Linear(3*3*32,128),
-            nn.ReLU(True),
-            nn.Linear(128, encoded_space_dim)
-        ) #Todo, layers, define according to some formula
-
-        pass
-
+        self.encoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
+                                               hidden_dim=num_frames,
+                                               kernel_size=(3, 3),
+                                               bias=True)
+        self.layers = [self.encoder_1_convlstm, self.encoder_1_convlstm]
     #Takes in features as well as the encoding size to use this time
     # INPUTS:
     #    x = Input Image
@@ -60,53 +38,46 @@ class Encoder(nn.Module):
     # OUTPUTS:
     #    encoded image: The actual encoded image to the proper size
     #    hidden:        Also returns the hidden state for future use if needed
-    def forward(self, x, enc_level:int, hidden=None):
+    def forward(self, x, enc_level:int, hidden=None, future_seq=0):
+        """
+        input: Tensor of shape (batch, time, channel, height, width)        #   batch, time, channel, height, width
+        """
+        # find size of different input dimensions
+        b, seq_len, _, h, w = x.size()
         assert enc_level < self.num_enc_layers
-        #Extract convolutional features
-        x = self.preproc_conv_net(x)
-        #Perform RNN on feature extraction, adds some context
-        x, hidden = self.rnn(x, hidden)
-        #Peform autoencoder downscaling to encoded version
 
-        # for i in range(0, enc_level): #Only run the first enc_level encoding layers
-        #     x = F.relu(self.layers[i](x))
-        for i in range(enc_level):
-            x = self.layers[i](x)
-        return x, hidden
+        h_t, c_t = self.encoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        h_t2, c_t2 = self.encoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        # encoder
+        for t in range(seq_len):
+            h_t, c_t = self.encoder_1_convlstm(input_tensor=x[:, t, :, :],
+                                               cur_state=[h_t, c_t])  # we could concat to provide skip conn here
+            h_t2, c_t2 = self.encoder_2_convlstm(input_tensor=h_t,
+                                                 cur_state=[h_t2, c_t2])  # we could concat to provide skip conn here
+
+        # encoder_vector
+        encoder_vector = h_t2
+        return encoder_vector
 
 class Decoder(nn.Module):
-    def __init__(self, mode:str, num_enc_layers:int):
+    def __init__(self, mode:str, num_enc_layers:int, num_frames):
         super().__init__()
-        #Define deconvolutional layers for reconstruction
-        self.conv_net = None # TODO
-        ## TODO pass encoding layers, variable encoding, convolutional LSTM
-        #Define an RNN to hold the memory state, this is based on the convolutional features
-        self.rnn = nn.Sequential(
-            nn.LSTM(750, 1000, num_layers=3),
-            nn.Linear(1000, 256),
-            nn.Linear(256,128)
-        ) # TODO
+        self.decoder_1_convlstm = ConvLSTMCell(input_dim=num_frames,  # nf + 1
+                                               hidden_dim=num_frames,
+                                               kernel_size=(3, 3),
+                                               bias=True)
 
-        #Define the autoencoder layers that increase size to enc_sizes[-1]
-        # This is some more potentially convolutional, not sure, preprocessing
-        initial_dim = [1,16,3]
-        self.num_enc_layers = num_enc_layers
-        self.layers = nn.Sequential(
-            nn.Linear(num_enc_layers, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 3*3*32),
-            nn.ReLU(True),
-            nn.Unflatten(dim=1, unflatten=(32,3,3)),
-            nn.ConvTranspose2d(32,16,3,stride=2,output_padding=0),
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(16,8,3,stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(8),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(initial_dim[1],initial_dim[0],initial_dim[2],stride=2,padding=1,output_padding=1)
-        ) #Todo, layers according to some formula
+        self.decoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
+                                               hidden_dim=num_frames,
+                                               kernel_size=(3, 3),
+                                               bias=True)
 
-        pass
+        self.decoder_CNN = nn.Conv3d(in_channels=num_frames,
+                                     out_channels=1,
+                                     kernel_size=(1, 3, 3),
+                                     padding=(0, 1, 1))
+
+        self.layers = [self.decoder_1_convlstm, self.decoder_2_convlstm, self.decoder_CNN]
 
      #Takes in encoded image as well as context / history
     # INPUTS:
@@ -115,20 +86,19 @@ class Decoder(nn.Module):
     # OUTPUTS:
     #    decoded image: The actual decoded image, reconstructed
     #    hidden:        Also returns the hidden state for future use if needed
-    def forward(self, x, hidden=None):
-        #Extract from x which encoding layer was the last used, we then use that to reverse it here
-        enc_level = None #TODO, extract from "x" to figure out its size
-        #Perform upscaling from whatever initial size was,
-        #we don't use the first enc_level layers since that is where we terminated encoding
-        # for i, size in range(enc_level, self.num_enc_layers):
-        #     x = F.relu(self.layers[i](x))
-        for i,_ in range(enc_level, self.num_enc_layers):
-            x = self.layers[i](x)
-        #It is now been upscaled to appropriate size, call RNN to add context
-        x, hidden = self.rnn(x, hidden)
-        #Perform final convolutional inverse to get original image
-        x = self.conv_net(x)
-        #Return
-        return x, hidden
+    def forward(self, x, future_step, hidden=None):
+        outputs = list()
+        for t in range(future_step):
+            h_t3, c_t3 = self.decoder_1_convlstm(input_tensor=encoder_vector,
+                                                 cur_state=[h_t3, c_t3])  # we could concat to provide skip conn here
+            h_t4, c_t4 = self.decoder_2_convlstm(input_tensor=h_t3,
+                                                 cur_state=[h_t4, c_t4])  # we could concat to provide skip conn here
+            encoder_vector = h_t4
+            outputs += [h_t4]  # predictions
 
-        pass
+        outputs = torch.stack(outputs, 1)
+        outputs = outputs.permute(0, 2, 1, 3, 4)
+        outputs = self.decoder_CNN(outputs)
+        outputs = torch.nn.Sigmoid()(outputs)
+
+        return outputs
