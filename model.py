f6de9dab7265437ea17a1e49ed141760aa1159b4
Ryan Ghosh <ryan.ghosh21@gmail.com>
2022-02-08 22:49:40 -0500

    Model running checkpoint, need to implement LSTM or RNN and fix output dim

--- model.py
+++ model.py
@@ -12,15 +12,22 @@ from ConvLSTM import ConvLSTMCell
 
 # Mode = ['Cutoff'] TODO, maybe also try dropout version
 # Enc_Sizes = Array of possible encoding size choices
-class Autoencoder():
+class Autoencoder(nn.Module):
     def __init__(self, num_enc_layers):
-        self.encoder = Encoder("default", num_enc_layers)
-        self.decoder = Decoder("default", num_enc_layers)
+        super().__init__()
+        self.encoder = Encoder(num_enc_layers)
+        self.decoder = Decoder(num_enc_layers)
 
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+
+        return x
 
 class Encoder(nn.Module):
-    def __init__(self, mode:str, num_enc_layers:int, num_frames=2):
+    def __init__(self, num_enc_layers:int, num_frames=2):
         super().__init__()
+        self.num_enc_layers = num_enc_layers
         # feature extraction taken from first few layers of VGG 16
         self.conv_preproc = nn.Sequential(
             nn.Conv2d(3,16,kernel_size=(3,3),padding=1),
@@ -34,15 +41,15 @@ class Encoder(nn.Module):
         )
 
         # ConvLSTM
-        self.encoder_1_convlstm = ConvLSTMCell(input_dim=3,
-                                               hidden_dim=num_frames,
-                                               kernel_size=(3, 3),
-                                               bias=True)
+        # self.encoder_1_convlstm = ConvLSTMCell(input_dim=3,
+        #                                        hidden_dim=num_frames,
+        #                                        kernel_size=(3, 3),
+        #                                        bias=True)
 
-        self.encoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
-                                               hidden_dim=num_frames,
-                                               kernel_size=(3, 3),
-                                               bias=True)
+        # self.encoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
+        #                                        hidden_dim=num_frames,
+        #                                        kernel_size=(3, 3),
+        #                                        bias=True)
 
         # DownSampling
         self.downsampling = [
@@ -61,34 +68,35 @@ class Encoder(nn.Module):
     # OUTPUTS:
     #    encoded image: The actual encoded image to the proper size
     #    hidden:        Also returns the hidden state for future use if needed
-    def forward(self, x, enc_level:int, hidden=None, future_seq=0):
+    def forward(self, x, hidden=None, future_seq=0):
         """
         input: Tensor of shape (batch, time, channel, height, width)        #   batch, time, channel, height, width
         """
         # find size of different input dimensions
         # b, seq_len, _, h, w = x.size()
-        assert enc_level < self.num_enc_layers
 
         ## begin feature extraction
         x = self.conv_preproc(x)
-        b, seq_len, _, h, w = x.size()
-        ## conv lstm
-        h_t, c_t = self.encoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
-        h_t2, c_t2 = self.encoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        # b, seq_len, _, h, w = x.size()
+        # ## conv lstm
+        # h_t, c_t = self.encoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        # h_t2, c_t2 = self.encoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
 
-        h_t, c_t = self.encoder_1_convlstm(input_tensor=x[:, 0, :, :],
-                                            cur_state=[h_t, c_t])  # we could concat to provide skip conn here
-        h_t2, c_t2 = self.encoder_2_convlstm(input_tensor=h_t,
-                                                cur_state=[h_t2, c_t2])  # we could concat to provide skip conn here
-        x = h_t2
+        # h_t, c_t = self.encoder_1_convlstm(input_tensor=x[:, 0, :, :],
+        #                                     cur_state=[h_t, c_t])  # we could concat to provide skip conn here
+        # h_t2, c_t2 = self.encoder_2_convlstm(input_tensor=h_t,
+        #                                         cur_state=[h_t2, c_t2])  # we could concat to provide skip conn here
+        # x = h_t2
+
         # begin downsampling with variable encoding
-        for i in range(enc_level):
+        for i in range(self.num_enc_layers):
             x = self.downsampling[i](x)
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, mode:str, num_enc_layers:int, num_frames=2):
+    def __init__(self, num_enc_layers:int, num_frames=2):
         super().__init__()
+        self.num_enc_layers = num_enc_layers
         self.upsample = [
             nn.Sequential(nn.ConvTranspose2d(3,8,kernel_size=(3,3),stride=2, padding=1), nn.ReLU(True)),
             nn.Sequential(nn.ConvTranspose2d(8,16,kernel_size=(3,3), padding=1), nn.ReLU(True)),
@@ -96,15 +104,15 @@ class Decoder(nn.Module):
             nn.Sequential(nn.ConvTranspose2d(32,64,kernel_size=(3,3), padding=1), nn.ReLU(True)),
             nn.Sequential(nn.ConvTranspose2d(64,3,kernel_size=(3,3),stride=2, padding=1), nn.ReLU(True))
         ]
-        self.decoder_1_convlstm = ConvLSTMCell(input_dim=3,  # nf + 1
-                                               hidden_dim=num_frames,
-                                               kernel_size=(3, 3),
-                                               bias=True)
+        # self.decoder_1_convlstm = ConvLSTMCell(input_dim=3,  # nf + 1
+        #                                        hidden_dim=num_frames,
+        #                                        kernel_size=(3, 3),
+        #                                        bias=True)
 
-        self.decoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
-                                               hidden_dim=num_frames,
-                                               kernel_size=(3, 3),
-                                               bias=True)
+        # self.decoder_2_convlstm = ConvLSTMCell(input_dim=num_frames,
+        #                                        hidden_dim=num_frames,
+        #                                        kernel_size=(3, 3),
+        #                                        bias=True)
 
         self.conv_postproc = nn.Sequential(
             nn.ConvTranspose2d(3,16,kernel_size=(3,3),padding=1),
@@ -123,25 +131,24 @@ class Decoder(nn.Module):
     # OUTPUTS:
     #    decoded image: The actual decoded image, reconstructed
     #    hidden:        Also returns the hidden state for future use if needed
-    def forward(self, x, num_enc_layers:int, future_seq=0, hidden=None):
-        outputs = list()
-        for i in range(num_enc_layers):
+    def forward(self, x, future_seq=0, hidden=None):
+        for i in range(len(self.upsample)-self.num_enc_layers, len(self.upsample)):
             x = self.upsample[i](x)
 
-        b, seq_len, _, h, w = x.size()
-        h_t3, c_t3 = self.decoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
-        h_t4, c_t4 = self.decoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        # b, seq_len, _, h, w = x.size()
+        # h_t3, c_t3 = self.decoder_1_convlstm.init_hidden(batch_size=b, image_size=(h, w))
+        # h_t4, c_t4 = self.decoder_2_convlstm.init_hidden(batch_size=b, image_size=(h, w))
 
-        h_t3, c_t3 = self.decoder_1_convlstm(input_tensor=x,
-                                                cur_state=[h_t3, c_t3])  # we could concat to provide skip conn here
-        h_t4, c_t4 = self.decoder_2_convlstm(input_tensor=h_t3,
-                                                cur_state=[h_t4, c_t4])  # we could concat to provide skip conn here
-        x = h_t4
-        outputs += [h_t4]  # predictions
+        # h_t3, c_t3 = self.decoder_1_convlstm(input_tensor=x,
+        #                                         cur_state=[h_t3, c_t3])  # we could concat to provide skip conn here
+        # h_t4, c_t4 = self.decoder_2_convlstm(input_tensor=h_t3,
+        #                                         cur_state=[h_t4, c_t4])  # we could concat to provide skip conn here
+        # x = h_t4
+        # outputs += [h_t4]  # predictions
 
-        outputs = torch.stack(outputs, 1)
-        outputs = outputs.permute(0, 2, 1, 3, 4)
-        outputs = self.conv_postproc(outputs)
-        outputs = torch.nn.Sigmoid()(outputs)
+        # outputs = torch.stack(outputs, 1)
+        # outputs = outputs.permute(0, 2, 1, 3, 4)
+        x = self.conv_postproc(x)
+        # outputs = torch.nn.Sigmoid()(outputs)
 
-        return outputs
+        return x
