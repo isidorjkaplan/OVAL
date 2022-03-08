4746a3ff99be09ffe2bd57c822ef73b118b01978
Isidor Kaplan <isidor.kaplan@utoronto.ca>
2022-03-08 18:43:06 -0500

    Showing all the loss functions instead of just one so we can compare

--- offline.py
+++ offline.py
@@ -53,7 +53,8 @@ def main_offline():
     device = 'cuda' if args.cuda else 'cpu'
 
     #Select the loss function
-    loss_fn = {'mse':F.mse_loss, 'mae':F.l1_loss, 'bce':nn.BCELoss()}[args.loss]
+    loss_functions = {'mse':F.mse_loss, 'mae':F.l1_loss, 'bce':nn.BCELoss()}
+    loss_fn = loss_functions[args.loss]
 
     #Load the model
     model = Autoencoder(video_size, save_path=args.save_model)
@@ -124,7 +125,9 @@ def main_offline():
 
             #Bookkeeping items
             epoch_loss.append(loss.item())
-            writer.add_scalar("Iter/train_loss", loss.item(), iter_num)
+            writer.add_scalar("Iter/primary_train_loss", loss.item(), iter_num)
+            for loss_key in loss_functions:
+                writer.add_scalar("Iter_Train_Loss/%s_loss" % loss_key, loss_functions[loss_key](frames_out,frames).detach().item(), iter_num)
 
             #uncomp_size = frames.numel()
             #comp_size = enc_frames.numel()*type_sizes[enc_frames.dtype]
@@ -150,7 +153,9 @@ def main_offline():
             frames_out = frames_out[:,:,:frames.shape[2],:frames.shape[3]]
             
             loss_v = loss_fn(frames_out, frames)
-            writer.add_scalar("Iter/valid_loss", loss_v.item(), iter_num)
+            writer.add_scalar("Iter/primary_valid_loss", loss_v.item(), iter_num)
+            for loss_key in loss_functions:
+                writer.add_scalar("Iter_Valid_Loss/valid_%s_loss" % loss_key, loss_functions[loss_key](frames_out,frames).detach().item(), iter_num)
 
             valid_loss.append(loss_v.item())
             print("%d: Video=%d, Frames=%d/%d=%d, loss_t=%g, loss_v=%g" % (iter_num, video_num, train_loader.num_frames_read, train_loader.total_num_frames, 100*train_loader.num_frames_read/train_loader.total_num_frames, loss.item(), loss_v.item()))
