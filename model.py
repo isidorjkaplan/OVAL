9e3d9c9affcb174a6352c066ca74a9645713ff32
Ryan Ghosh <ryan.ghosh21@gmail.com>
2022-03-08 22:33:38 -0500

    offline Encoder variable encoding done

--- model.py
+++ model.py
@@ -15,10 +15,15 @@ ConvSettings = namedtuple("ConvSettings", "out_channels stride kern")
 
 #TEMPORRY, WILL REPLACE WITH RYANS AUTOENCODER MODEL LATER
 class Autoencoder(nn.Module):
-    def __init__(self, image_dim, n_channels=3, save_path=None, conv_settings=[ConvSettings(6, 1, 3), ConvSettings(8, 1, 3), ConvSettings(9, 2, 3), ConvSettings(10, 2, 4), ConvSettings(10, 2, 4),ConvSettings(10, 2, 4), ConvSettings(10, 2, 3)], linear_layers=None):
+    def __init__(
+        self, image_dim, num_enc_layers, n_channels=3, save_path=None,
+        conv_settings=[ConvSettings(6, 1, 3), ConvSettings(8, 1, 3), ConvSettings(9, 2, 3), ConvSettings(10, 2, 4), ConvSettings(10, 2, 4),ConvSettings(10, 2, 4), ConvSettings(10, 2, 3)],
+        enc_conv=[ConvSettings(4,2,3), ConvSettings(8,1,3), ConvSettings(10,2,3), ConvSettings(10,1,3), ConvSettings(10,2,3)],
+        linear_layers=None
+        ):
         super().__init__()
-        self.encoder = Encoder(image_dim, n_channels, conv_settings, linear_layers)
-        self.decoder = Decoder(image_dim, n_channels, conv_settings, linear_layers, self.encoder.flatten_size, self.encoder.conv_out_shape)
+        self.encoder = Encoder(image_dim, n_channels, conv_settings, linear_layers, num_enc_layers)
+        self.decoder = Decoder(image_dim, n_channels, conv_settings, linear_layers, self.encoder.flatten_size, self.encoder.conv_out_shape, num_enc_layers)
         self.save_path = save_path
 
         print(self)
@@ -38,9 +43,9 @@ class Autoencoder(nn.Module):
         self.decoder.to(device)
 
     #TODO load_state_dict
-    
+
 class Encoder(nn.Module):
-    def __init__(self, image_dim, n_channels, conv_settings, linear_layers):
+    def __init__(self, image_dim, n_channels, conv_settings, linear_layers, num_enc_layers, enc_conv):
         super().__init__()
         self.conv_net = [nn.Conv2d(n_channels, conv_settings[0].out_channels, kernel_size=conv_settings[0].kern, stride=conv_settings[0].stride)]
         [self.conv_net.extend([nn.ReLU(inplace=True), nn.Conv2d(conv_settings[i-1].out_channels, conv_settings[i].out_channels, kernel_size=conv_settings[i].kern, stride=conv_settings[i].stride)]) for i in range(1, len(conv_settings))]
@@ -61,6 +66,10 @@ class Encoder(nn.Module):
             [layers.extend([nn.ReLU(inplace=True), nn.Linear(linear_layers[i-1], linear_layers[i])]) for i in range(1, len(linear_layers))]
 
             self.linear_net = nn.Sequential(*layers)
+        self.num_enc_layers = num_enc_layers
+        self.enc_conv = [nn.Conv2d(conv_settings[-1].out_channels,kernel_size=enc_conv[0].kern, stride=enc_conv[0].stride)]
+        [self.enc_conv.extend([nn.ReLU(inplace=True), nn.Conv2d(enc_conv[i-1].out_channels, enc_conv[i].out_channels, kernel_size=enc_conv[i].kern, stride=enc_conv[i].stride)]) for i in range(1, len(enc_conv))]
+        self.enc_conv = nn.Sequential(*self.enc_conv)
 
     def forward(self, x, hidden=None):
         assert (x.shape[2], x.shape[3]) == self.image_dim
@@ -75,12 +84,12 @@ class Encoder(nn.Module):
 
         if self.has_linear:
             x = self.linear_net(x.view(x.shape[0], self.flatten_size))
-
+        x = self.enc_conv(x)
         return x, hidden
 
-        
+
 class Decoder(nn.Module):
-    def __init__(self, image_dim, n_channels, conv_settings, linear_layers, flatten_size, conv_shape):
+    def __init__(self, image_dim, n_channels, conv_settings, linear_layers, flatten_size, conv_shape, num_enc_layers):
         super().__init__()
         self.has_linear = linear_layers is not None
         if self.has_linear:
@@ -100,17 +109,17 @@ class Decoder(nn.Module):
 
         self.image_dim = image_dim
         self.conv_shape = conv_shape
-
+        self.num_enc_layers = num_enc_layers
 
     def forward(self, x, hidden=None):
         if self.has_linear:
             x = self.linear_net(x)
             x = x.view(x.shape[0], 1, *self.conv_shape)
-        
+
         if hidden is not None:
             hidden = [[x.detach() for x in y] for y in hidden]
         x, hidden = self.convLSTM(x.view(1, *x.shape), hidden)
-        x = x[-1][0] 
+        x = x[-1][0]
         #x = x[-1][:,0,:,:,:]
 
         x = self.conv_net(x)
