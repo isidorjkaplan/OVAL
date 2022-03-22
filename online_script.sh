b69bedcfe5b4f82b091cdc16f05edaffef83c7ca
Isidor Kaplan <isidor.kaplan@utoronto.ca>
2022-03-22 19:06:35 -0400

    Adding relative update to the automatic script generator and rerunning to get some new model results with the test metric for updates

--- online_script.sh
+++ online_script.sh
@@ -5,5 +5,6 @@ for f in $FILES
 do
     NAME="$(basename $f)"
     echo "Processing $NAME file..."
-    python online.py --load_model=data/models/offline8.pt --video=$f --out=data/videos/out/online_$NAME --cuda 
+    python online.py --load_model=data/models/offline8.pt --video=$f --out=data/videos/out/online_noupdate_$NAME --cuda --update_err=1000
+    python online.py --load_model=data/models/offline8.pt --video=$f --out=data/videos/out/online_withupdate_$NAME --cuda --update_err=0.01
 done
